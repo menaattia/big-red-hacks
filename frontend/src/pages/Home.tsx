e48a0ac8b4f74d49067bdbfdd4a9ed8b4d5b6ff5
nada-attia <nma46@cornell.edu>
2022-10-15 08:25:08 -0400

    Add dummy data and load the data into the graphs for each category

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,26 +1,46 @@
 import * as React from 'react';
+import {useState} from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Tabs from '../components/Tabs'
 import Graph from '../components/Graph';
+import Data from '../Data'
 
 const HomePage = () => {
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container fixed>
-        <Box
-        sx={{
-            width: 500,
-            height: 250
-        }}
-        />
-        <Tabs></Tabs>
-        <Graph></Graph>
-      </Container>
-    </React.Fragment>
-  );
+    const [tab, setTab] = useState<string>("recycling");
+
+    const handleTab = (tabValue:string) => {
+        setTab(tabValue)
+    } 
+
+    const getData = () => {
+        if (tab === "biking") {
+            return Data.biking
+        } else if (tab === "walking") {
+            return Data.walking
+        } else if (tab === "zero-waste") {
+            return Data.zero_waste
+        } else {
+            return Data.recycling
+        }
+    }
+
+    return (
+        <React.Fragment>
+        <CssBaseline />
+        <Container fixed>
+            <Box
+            sx={{
+                width: 500,
+                height: 250
+            }}
+            />
+            <Tabs handleTab={handleTab}></Tabs>
+            <Graph data={getData()}></Graph>
+        </Container>
+        </React.Fragment>
+    );
 }
 
 export default HomePage;
