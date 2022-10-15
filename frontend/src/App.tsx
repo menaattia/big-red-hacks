3176a464d612017ec83149b6f55ea9c1dd5a6386
Mena Attia <mena.attia.ma@gmail.com>
2022-10-14 20:49:41 -0400

    set up routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home';
 import './App.css';
 
 function App() {
   return (
-    <HomePage />
+    <div className='app'>
+      <Router>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
