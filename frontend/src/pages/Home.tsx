a34174ae1c322069402cb5bf9b42a859d5540696
Mohammad Labadi <78539464+mohdLabadi@users.noreply.github.com>
2022-10-15 15:42:33 -0400

    message

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 
 const HomePage = () => {
- return <h1>Hackathon Home Page</h1>
+ return <div> <h1>Hackathon Home Page is blank</h1>
 
+            <h2> Team NullPointException </h2>
+
+            
+ 
+        </div>
+        
+        
 }
+
+
 
 export default HomePage;
