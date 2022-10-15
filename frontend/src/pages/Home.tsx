245bfe4ddf21338533aa061a24f87c033af09e62
nada-attia <nma46@cornell.edu>
2022-10-14 22:10:49 -0400

    Add tabs

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Tabs from '../components/Tabs'
 
 const HomePage = () => {
- return <h1>Hackathon Home Page</h1>
-
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <Box
+        sx={{
+            width: 500,
+            height: 250
+        }}
+        />
+        <Tabs></Tabs>
+      </Container>
+    </React.Fragment>
+  );
 }
 
 export default HomePage;
